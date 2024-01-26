b3f6f617021b26aead88adae749aa046bdff70e4
hyunsooju <hyunsooting@gmail.com>
2024-01-26 11:56:33 +0900

    added text comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("It's a beautiful life"));
+app.get('/', (req, res) => res.send("It's a miserable life"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
